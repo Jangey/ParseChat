524a96c62961f09e3417d28bfaf7c2e4e43db13d
Jangey <jangey.lu@gmail.com>
2018-09-23 02:12:36 -0700

    Finish Username and Password signup

--- Parse Chat/LoginViewController.swift
+++ Parse Chat/LoginViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
+    
+    let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: .alert)
     
     
     override func viewDidLoad() {
@@ -23,9 +26,37 @@ class LoginViewController: UIViewController {
 
     
     @IBAction func tapSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground { (success: Bool
+            , error: Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("User Registered successfully")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
     }
     
     @IBAction func tapLogin(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?
+            , error: Error?) in
+            if user != nil {
+                print("Loged in Success!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+        
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            // handle response here.
+        }
+        // add the OK action to the alert controller
+        alertController.addAction(OKAction)
     }
     
 }
