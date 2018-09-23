72aadbde332c0aa1d15712d43b04a1fbb6c02d44
Jangey <jangey.lu@gmail.com>
2018-09-23 02:02:55 -0700

    Add Login Page Button

---
+++ Parse Chat/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Parse Chat
+//
+//  Created by Jangey Lu on 9/23/18.
+//  Copyright © 2018 Jangey Lu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func tapSignUp(_ sender: Any) {
+    }
+    
+    @IBAction func tapLogin(_ sender: Any) {
+    }
+    
+}
