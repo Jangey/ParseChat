223ec6a83b090468eca816726ec7f757b54f3adc
Jangey <jangey.lu@gmail.com>
2018-09-23 18:25:58 -0700

    Milestone 4: View a List of Chat Messages

---
+++ Parse Chat/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  Parse Chat
+//
+//  Created by Jangey Lu on 9/23/18.
+//  Copyright © 2018 Jangey Lu. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Parse Chat/ChatViewController.swift
+++ Parse Chat/ChatViewController.swift
@@ -9,19 +9,25 @@
 import UIKit
 import Parse
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var chatMessageField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let chatMessage = PFObject(className: "Message")
+    var messages: [PFObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // refresh each second
+        onTimer()
     }
     
     @IBAction func tapSend(_ sender: Any) {
         
-         let chatMessage = PFObject(className: "Message")
          chatMessage["text"] = chatMessageField.text ?? ""
          chatMessage.saveInBackground { (success, error) in
          if success {
@@ -43,6 +49,42 @@ class ChatViewController: UIViewController {
         appDelegate?.window??.rootViewController = signInPage
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
     
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+        
+        //cell.business = businesses[indexPath.row]
+        
+        return cell
+    }
+    
+    @objc func onTimer() {
+        // Add code to be run periodically
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.onTimer), userInfo: nil, repeats: true)
+        queryMessage()
+    }
+    
+    func queryMessage(){
+        // construct query
+        let query = PFQuery(className: "Message")
+        //query.addDescendingOrder("createdAt")
+        //query.includeKey("user")
+        query.addDescendingOrder("createdAt")
+        query.includeKey("user")
+        // fetch data asynchronously
+        query.findObjectsInBackground { (posts: [PFObject]?, error: Error?) in
+            if let posts = posts {
+                for message in posts{
+                    print(message["text"])
+                }
+                self.messages = posts
+                self.tableView.reloadData()
+            } else {
+                print(error!.localizedDescription)
+            }
+        }
+    }
 }
